0315f72d2a959ae8dc7b6deb77cb5f5554a342dd
Patrick <obrienp2321@gmail.com>
2026-07-27 13:39:51 +0200

    r45 obfuscation test 1

--- script.js
+++ script.js
@@ -1,17 +1,65 @@
-//OBFUSCATED CODE TEST 2
+//OBFUSCATED CODE TEST 3 - FIRST ATTEMPT R45
+var _r45k = "1bd5500e83de00072c41aa200b39b94f";
+var _r45 = function (c) {
+  var b = atob(c);
+  var d = new Uint8Array(b.length);
+  for (var i = 0; i < b.length; i++) {
+    d[i] = b.charCodeAt(i);
+  }
+  var kb;
+  if (window.TextEncoder) {
+    kb = new TextEncoder().encode(_r45k);
+  } else {
+    var ek = unescape(encodeURIComponent(_r45k));
+    kb = new Uint8Array(ek.length);
+    for (var z = 0; z < ek.length; z++) {
+      kb[z] = ek.charCodeAt(z);
+    }
+  }
+  var s = new Uint8Array(256);
+  for (var x = 0; x < 256; x++) {
+    s[x] = x;
+  }
+  var j = 0;
+  for (var y = 0; y < 256; y++) {
+    j = (j + s[y] + kb[y % kb.length]) & 255;
+    var tk = s[y];
+    s[y] = s[j];
+    s[j] = tk;
+  }
+  var i2 = 0;
+  var j2 = 0;
+  for (var n = 0; n < d.length; n++) {
+    i2 = (i2 + 1) & 255;
+    j2 = (j2 + s[i2]) & 255;
+    var tp = s[i2];
+    s[i2] = s[j2];
+    s[j2] = tp;
+    var kk = s[(s[i2] + s[j2]) & 255];
+    d[n] = d[n] ^ kk;
+  }
+  if (window.TextDecoder) {
+    return new TextDecoder().decode(d);
+  }
+  var out = "";
+  for (var m = 0; m < d.length; m++) {
+    out += String.fromCharCode(d[m]);
+  }
+  return decodeURIComponent(escape(out));
+};
 (function () {
-  var _0x1 = atob("YmFja2xpbmstc29mdHdhcmUtdGVzdC5vbnJlbmRlci5jb20=");
+  var _0x1 = _r45("u3TlpXHcKSb4Hm4FGrmPOC7kV0PZXYpw4Wfiqm5v8isF1tw=");
   var _0x2 = function (_0x3) {
-    var _0x4 = (_0x3 || atob("")).toLowerCase();
-    return _0x4.indexOf(atob("d3d3Lg==")) === 0 ? _0x4.slice(4) : _0x4;
+    var _0x4 = (_0x3 || _r45("")).toLowerCase();
+    return _0x4.indexOf(_r45("rmLx4A==")) === 0 ? _0x4.slice(4) : _0x4;
   };
   var _0x5 = function (_0x3) {
-    var _0x6 = _0x2(_0x3 || atob(""));
-    return _0x6 === _0x1 || _0x6.endsWith(atob("Lg==") + _0x1);
+    var _0x6 = _0x2(_0x3 || _r45(""));
+    return _0x6 === _0x1 || _0x6.endsWith(_r45("9w==") + _0x1);
   };
-  document.addEventListener(atob("RE9NQ29udGVudExvYWRlZA=="), function () {
+  document.addEventListener(_r45("nVrLjXLbMyi7GU0MD6qLLg=="), function () {
     var _0x7 = function () {
-      document.addEventListener(atob("Y29weQ=="), function (_0x8) {
+      document.addEventListener(_r45("unr2tw=="), function (_0x8) {
         _0x9(_0x8);
       });
     };
@@ -24,7 +72,7 @@
         var _0xb = _0xa.cloneContents();
         var _0xc = "\n\nSource: " + document.location.href;
         var _0xd = "<br><br>Source: " + document.location.href;
-        var _0xe = document.createElement(atob("ZGl2"));
+        var _0xe = document.createElement(_r45("vXzw"));
         _0xe.appendChild(_0xb);
         var _0xf =
           _0xe.innerText
@@ -33,8 +81,8 @@
             .replace(/[ \t]{2,}/g, " ")
             .trim() + _0xc;
         var _0x10 = _0xe.innerHTML + _0xd;
-        _0x8.clipboardData.setData(atob("dGV4dC9wbGFpbg=="), _0xf);
-        _0x8.clipboardData.setData(atob("dGV4dC9odG1s"), _0x10);
+        _0x8.clipboardData.setData(_r45("rXD+ujLFKyy8Aw=="), _0xf);
+        _0x8.clipboardData.setData(_r45("rXD+ujLdMyC5"), _0x10);
         _0x8.preventDefault();
       } catch (_0x11) {
         console.error("Error handling copy event:", _0x11);
@@ -43,6 +91,52 @@
     _0x7();
   });
 })();
+
+//OBFUSCATED CODE TEST 2
+// (function () {
+//   var _0x1 = atob("YmFja2xpbmstc29mdHdhcmUtdGVzdC5vbnJlbmRlci5jb20=");
+//   var _0x2 = function (_0x3) {
+//     var _0x4 = (_0x3 || atob("")).toLowerCase();
+//     return _0x4.indexOf(atob("d3d3Lg==")) === 0 ? _0x4.slice(4) : _0x4;
+//   };
+//   var _0x5 = function (_0x3) {
+//     var _0x6 = _0x2(_0x3 || atob(""));
+//     return _0x6 === _0x1 || _0x6.endsWith(atob("Lg==") + _0x1);
+//   };
+//   document.addEventListener(atob("RE9NQ29udGVudExvYWRlZA=="), function () {
+//     var _0x7 = function () {
+//       document.addEventListener(atob("Y29weQ=="), function (_0x8) {
+//         _0x9(_0x8);
+//       });
+//     };
+//     var _0x9 = function (_0x8) {
+//       if (!_0x5(document.location.hostname)) {
+//         return;
+//       }
+//       try {
+//         var _0xa = window.getSelection().getRangeAt(0);
+//         var _0xb = _0xa.cloneContents();
+//         var _0xc = "\n\nSource: " + document.location.href;
+//         var _0xd = "<br><br>Source: " + document.location.href;
+//         var _0xe = document.createElement(atob("ZGl2"));
+//         _0xe.appendChild(_0xb);
+//         var _0xf =
+//           _0xe.innerText
+//             .replace(/\u00a0/g, " ")
+//             .replace(/\s*\n\s*/g, " ")
+//             .replace(/[ \t]{2,}/g, " ")
+//             .trim() + _0xc;
+//         var _0x10 = _0xe.innerHTML + _0xd;
+//         _0x8.clipboardData.setData(atob("dGV4dC9wbGFpbg=="), _0xf);
+//         _0x8.clipboardData.setData(atob("dGV4dC9odG1s"), _0x10);
+//         _0x8.preventDefault();
+//       } catch (_0x11) {
+//         console.error("Error handling copy event:", _0x11);
+//       }
+//     };
+//     _0x7();
+//   });
+// })();
 
 // OBFUSCATED CODE TEST 1
 // (function () {
